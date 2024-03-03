4437d340e8d83da42d2d0e35aa3d3d37751390e1
Dynavx <haical31@gmail.com>
2024-03-03 19:02:34 +0700

    first commit

---
+++ FirstAPP/FirstAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAPPApp.swift
+//  FirstAPP
+//
+//  Created by Heical Chandra on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
